293a681d6eef8e26f6a58b14a017aa7eeee0a0e2
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-07-30 15:18:29 +0200

    Improve variable names and add more tests
    @aramgrigoryan I’ve added more tests, but they don’t pass. Next week
    we’re going to go over unit testing. So we can write them in a
    dedicated place outside of our code, and have Travis run them
    automatically.

--- src/bio2bel_interpro/tree.py
+++ src/bio2bel_interpro/tree.py
@@ -1,21 +1,34 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 import networkx as nx
+
 from pybel.constants import PYBEL_DATA_DIR
 from pybel.utils import ensure_quotes
 from pybel_tools.document_utils import write_boilerplate
 from pybel_tools.resources import get_latest_arty_namespace
 
-CHEMBL_DATA_DIR = os.path.join(PYBEL_DATA_DIR, 'bio2bel', 'chembl')
-if not os.path.exists(CHEMBL_DATA_DIR):
-    os.makedirs(CHEMBL_DATA_DIR)
+INTERPRO_DATA_DIR = os.path.join(PYBEL_DATA_DIR, 'bio2bel', 'interpro')
+if not os.path.exists(INTERPRO_DATA_DIR):
+    os.makedirs(INTERPRO_DATA_DIR)
 
-TREE_FILE_PATH = os.path.join(CHEMBL_DATA_DIR, 'ParentChildTreeFile.txt')
+TREE_FILE_PATH = os.path.join(INTERPRO_DATA_DIR, 'ParentChildTreeFile.txt')
 
 
-def read_tree(G, file, option, parent=None, depth=0):
+def read_tree(graph, file, option, parent=None, depth=0):
+    """
+
+    :param graph:
+    :param file:
+    :param option:
+    :param parent:
+    :param depth:
+    :return:
+    """
     line = next(file)
     word = '::'.join(line.split('::')[:2])
+
     while word:
         dashes = word.count('-') // 2
         word = word.lstrip('-')
@@ -24,36 +37,54 @@ def read_tree(G, file, option, parent=None, depth=0):
 
         if dashes >= depth:
             if parent is not None:
-                G.add_edge(parent.split('::')[option], word.split('::')[option])
-            word = read_tree(G, file, option, word, depth + 1)
+                graph.add_edge(parent.split('::')[option], word.split('::')[option])
+            word = read_tree(graph, file, option, word, depth + 1)
         else:
-            word = read_tree(G, file, option, parent, depth)
+            word = read_tree(graph, file, option, parent, depth)
 
 
-def main(file):
-    G = nx.DiGraph()
-    opt = 1 #1 for name 0 for ID
+def main(file, opt=1):
+    """
+
+    :param file:
+    :param opt:
+    :return:
+    :rtype: networkx.DiGraph
+    """
+    assert opt in {0, 1}
+
+    graph = nx.DiGraph()
     try:
-        read_tree(G, file, opt, parent=None, depth=0)
+        read_tree(graph, file, opt, parent=None, depth=0)
     except:
         pass
+
     if opt == 0:
-        assert 'IPR002420' in G.edge['IPR000008']
-        assert 'IPR001840' in G.edge['IPR018081']
-        assert 'IPR033965' in G.edge['IPR000092']
+        assert 'IPR002420' in graph.edge['IPR000008']
+        assert 'IPR001840' in graph.edge['IPR018081']
+        assert 'IPR033965' in graph.edge['IPR000092']
     else:
-        if opt == 1:
-            assert 'Phosphatidylinositol 3-kinase, C2 domain' in G.edge['C2 domain']
-        else:
-            assert opt == 0 or opt == 1
+        assert 'Phosphatidylinositol 3-kinase, C2 domain' in graph.edge['C2 domain']
 
-    return G
+        assert 'Histamine H1 receptor' in graph.edge['G protein-coupled receptor, rhodopsin-like']
+        assert not graph.edge['Histamine H1 receptor']
+
+        assert 'Dopamine receptor family' in graph.edge['G protein-coupled receptor, rhodopsin-like']
+        assert 'Dopamine D1 receptor, C2 domain' in graph.edge['Dopamine receptor family']
+        assert not graph.edge['Dopamine D1 receptor, C2 domain']
+        assert 'Dopamine D2 receptor, C2 domain' in graph.edge['Dopamine receptor family']
+        assert 'Dopamine D3 receptor, C2 domain' in graph.edge['Dopamine receptor family']
+        assert 'Dopamine D4 receptor, C2 domain' in graph.edge['Dopamine receptor family']
+        assert 'Dopamine D5 receptor, C2 domain' in graph.edge['Dopamine receptor family']
+        assert not graph.edge['Dopamine D5 receptor, C2 domain']
+
+    return graph
 
 
-def make_bel(G, file):
+def make_bel(graph, file):
     """Creates the lines of BEL document that represents the InterPro hierarchy
 
-    :param networkx.DiGraph G:
+    :param networkx.DiGraph graph: A graph representing the InterPro hierarchy from :func:`main`
     :return: An iterator over the lines of a BEL document
     :rtype: iter[str]
     """
@@ -75,7 +106,7 @@ def make_bel(G, file):
         file=file
     )
 
-    for parent, child in G.edges_iter():
+    for parent, child in graph.edges_iter():
         print('p(INTERPRO:{}) isA p(INTERPRO:{})'.format(
             ensure_quotes(child),
             ensure_quotes(parent),
@@ -83,10 +114,16 @@ def make_bel(G, file):
 
 
 def write_interpro_bel(in_file, file=None):
+    """
+
+    :param file in_file:
+    :param file file:
+    :return:
+    """
     G = main(in_file)
     make_bel(G, file)
 
 
 if __name__ == '__main__':
-    with open(TREE_FILE_PATH, 'r') as f, open(os.path.join(CHEMBL_DATA_DIR, 'blahblah.bel'), 'w') as f2:
+    with open(TREE_FILE_PATH, 'r') as f, open(os.path.join(INTERPRO_DATA_DIR, 'interpro_hierarchy.bel'), 'w') as f2:
         write_interpro_bel(f, f2)
