3a377ae365a5032856175b44c4c1c65f354961ab
aramgrigoryan <aramgrigoryan@protonmail.ch>
2017-09-05 14:43:12 +0200

    finishing get_data() method

--- src/bio2bel_interpro/interpro_to_go.py
+++ src/bio2bel_interpro/interpro_to_go.py
@@ -2,6 +2,7 @@
 
 import os
 import pandas as pd
+import re
 from urllib.request import urlretrieve
 
 from pybel.constants import PYBEL_DATA_DIR
@@ -16,8 +17,8 @@ if not os.path.exists(INTERPRO_DATA_DIR):
 URL = 'ftp://ftp.ebi.ac.uk/pub/databases/interpro/interpro2go'
 MAPPING_FILE = os.path.join(INTERPRO_DATA_DIR, 'interpro2go')
 
-INTERPRO_REGEX = '(INTERPRO:IPR)(\d+)'
-GO_REGEX = '(GO:)(\d+)'
+INTERPRO_REGEX = '(InterPro)(.)(IPR)(\d+)'
+GO_REGEX = '(GO)(.)(\d+)($)'
 
 def download_mapping_file(force=False):
     """Downloads Mapping file if not found on file system
@@ -29,14 +30,28 @@ def download_mapping_file(force=False):
     elif not os.path.isfile(MAPPING_FILE):
         urlretrieve(URL, MAPPING_FILE)
 
-download_mapping_file()
 
 def get_data():
     """
     Extracts mapping data from file and returns the Data as Data Frame
     :return: pandas.DataFrame : Data as Data Frame
     """
+    download_mapping_file()
+    data = []
+
     with open(MAPPING_FILE, 'r') as file:
         for line in file:
             if line[0] == '!':
                 continue
+            go_id = re.search(GO_REGEX, line).group().split(':')[1]
+            interpro_id = re.search(INTERPRO_REGEX, line).group().split(':')[1]
+            data.append([interpro_id, go_id])
+    df = pd.DataFrame(data, columns=['interpro_id', 'go_id'], dtype=str)
+    return df
+
+
+
+
+if __name__ == '__main__':
+    df = get_data()
+    print(df.head())
