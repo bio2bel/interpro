c6f49a6db1807d3a3b2323d688daf48c7d0bc3a1
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-05 18:06:46 +0100

    Improve setup and requirements

--- setup.py
+++ setup.py
@@ -9,8 +9,10 @@ import setuptools
 PACKAGES = setuptools.find_packages(where='src')
 META_PATH = os.path.join('src', 'bio2bel_interpro', '__init__.py')
 INSTALL_REQUIRES = [
-    'pybel_tools',
+    'pybel',
     'networkx',
+    'click',
+    'sqlalchemy',
     'pandas',
     'fuckit',
 ]
