94edc0ce53805a3e08d29a4c882a10f3414d45a8
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-05 18:03:08 +0100

    Remove pybel_tools reference

--- src/bio2bel_interpro/enrich.py
+++ src/bio2bel_interpro/enrich.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from pybel_tools import pipeline
-
-
-@pipeline.in_place_mutator
 def enrich_proteins(graph):
     """Adds the InterPro annotations for proteins in the graph
 
     :param pybel.BELGraph graph: A BEL Graph
     """
     raise NotImplemented
-
