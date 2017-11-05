db04e4fd18be5074061767f718cfb102f6016a57
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-05 18:04:58 +0100

    Code cleanup
    - moved constants to constants.py
    - fixed references to pybel and pybeltools
    - refactored logic in download mapping file function and renamed
    - better keyword arguments and documentation for force downloading
    - pre-compile regular expressions
    - use %s in logging because it’s more efficient and looks nicer
    (normally {} is better though)
    - Fixed relationship type in serialization and function for GO

--- src/bio2bel_interpro/interpro_to_go.py
+++ src/bio2bel_interpro/interpro_to_go.py
@@ -6,93 +6,98 @@ import re
 from urllib.request import urlretrieve
 
 import pandas as pd
-from pybel.constants import PYBEL_DATA_DIR
+
+from bio2bel_interpro.constants import INTERPRO_DATA_DIR
+from pybel.resources.document import make_knowledge_header
 from pybel.utils import ensure_quotes
-from pybel_tools.document_utils import write_boilerplate
 
 log = logging.getLogger(__name__)
 
-INTERPRO_DATA_DIR = os.path.join(PYBEL_DATA_DIR, 'bio2bel', 'interpro')
-if not os.path.exists(INTERPRO_DATA_DIR):
-    os.makedirs(INTERPRO_DATA_DIR)
-
-URL = 'ftp://ftp.ebi.ac.uk/pub/databases/interpro/interpro2go'
-MAPPING_FILE = os.path.join(INTERPRO_DATA_DIR, 'interpro2go')
-DESTINATION_FILE = os.path.join(INTERPRO_DATA_DIR, 'interpro_to_go.bel')
+INTERPRO_GO_MAPPING_URL = 'ftp://ftp.ebi.ac.uk/pub/databases/interpro/interpro2go'
+#: Local download location for interpro2go mapping
+INTERPRO_GO_MAPPING_PATH = os.path.join(INTERPRO_DATA_DIR, 'interpro2go.txt')
 
 INTERPRO_REGEX = '(InterPro)(.)(IPR)(\d+)'
+INTERPRO_REGEX_COMPILED = re.compile(INTERPRO_REGEX)
 GO_REGEX = '(GO)(.)(\d+)($)'
+GO_REGEX_COMPILED = re.compile(GO_REGEX)
 
 
-def download_mapping_file(force=False):
+def ensure_interpro_go_mapping_file(force_download=False):
     """Downloads Mapping file if not found on file system
 
-    :param bool force: Forces the download
+    :param bool force_download: Forces the download
     """
-    if force:
-        urlretrieve(URL, MAPPING_FILE)
-    elif not os.path.isfile(MAPPING_FILE):
-        urlretrieve(URL, MAPPING_FILE)
+    if force_download or not os.path.exists(INTERPRO_GO_MAPPING_PATH):
+        log.info('downloading interpro2go mapping from %s', INTERPRO_GO_MAPPING_URL)
+        urlretrieve(INTERPRO_GO_MAPPING_URL, INTERPRO_GO_MAPPING_PATH)
 
 
-def get_data():
-    """
-    Extracts mapping data from file and returns the Data as Data Frame
+def get_interpro_go_mapping(force_download=False):
+    """ Extracts mapping data from file and returns the Data as Data Frame
+
     :return: pandas.DataFrame : Data as Data Frame
     """
-    download_mapping_file()
+    ensure_interpro_go_mapping_file(force_download=force_download)
     data = []
 
-    with open(MAPPING_FILE, 'r') as file:
+    with open(INTERPRO_GO_MAPPING_PATH, 'r') as file:
         for line in file:
             if line[0] == '!':
                 continue
-            go_id = re.search(GO_REGEX, line).group().split(':')[1]
-            interpro_id = re.search(INTERPRO_REGEX, line).group().split(':')[1]
+
+            go_id = GO_REGEX_COMPILED.search(line).group().split(':')[1]
+            interpro_id = INTERPRO_REGEX_COMPILED.search(line).group().split(':')[1]
             data.append([interpro_id, go_id])
+
     df = pd.DataFrame(data, columns=['interpro_id', 'go_id'], dtype=str)
-    log.info("\n{}".format(df.head()))
-    log.info("\n{} entries in total".format(len(df)))
+
+    log.info("Head:\n %s", df.head())
+    log.info('entries in total: %d', len(df))
+
     return df
 
 
-def write_interpro_to_go_belns(file, df=None):
-    """
-    WRITES BEL MAPPING NAMESPACE FILE
-    :return: None
-    """
-    df = get_data() if df is None else df
+def write_interpro_to_go_bel(file=None, df=None, force_download=False):
+    """Converts the InterPro to GO mapping to BEL
 
-    write_boilerplate(
-        document_name='InterPro to GO equivalence',
+    :param file:
+    :param df:
+    :param bool force_download:
+    """
+    df = get_interpro_go_mapping(force_download=force_download) if df is None else df
+
+    lines = make_knowledge_header(
+        name='InterPro to GO equivalence',
         authors='Aram Grigoryan',
         contact='aram.grigoryan@scai.fraunhofer.de',
         licenses='Creative Commons by 4.0',
         copyright='Copyright (c) 2017 Aram Grigoryan. All Rights Reserved.',
         description="""This BEL document represents InterPro to GO strings equivalence""",
-        namespace_dict={
+        namespace_url={
             'InterPro': INTERPRO_REGEX,
             'GO': GO_REGEX,
         },
         namespace_patterns={},
-        annotations_dict={},
-        annotations_patterns={},
-        file=file
+        annotation_url={},
+        annotation_patterns={},
     )
 
-    print('SET Citation = {{"URL","{}"}}'.format(
-        "https://academic.oup.com/nar/article/45/D1/D190/2605789/InterPro-in-2017-beyond-protein-family-and-domain"),
-          file=file)
+    for line in lines:
+        print(line, file=file)
+
+    print('SET Citation = {"PubMed","27899635"}', file=file)
     print('SET Evidence = "InterPro to GO mapping"', file=file)
 
     for _, interpro_id, go_id in df[['interpro_id', 'go_id']].itertuples():
         interpro_clean = ensure_quotes(str(interpro_id).strip())
         go_clean = ensure_quotes(str(go_id).strip())
-        print('p(InterPro:{}) eq p(GO:{})'.format(interpro_clean, go_clean), file=file)
+        print('p(InterPro:{}) -- bp(GO:{})'.format(interpro_clean, go_clean), file=file)
 
 
 if __name__ == '__main__':
-    log.setLevel(20)
-    logging.basicConfig(level=20)
-    with open(DESTINATION_FILE, 'w+') as f:
-        write_interpro_to_go_belns(f)
+    logging.basicConfig(level=10)
+    log.setLevel(10)
+
+    with open(os.path.expanduser('~/Desktop/interpro2go.bel'), 'w') as f:
+        write_interpro_to_go_bel(f)
