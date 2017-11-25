be9707a3bf479a0b36bf6af3b5d52775740e9331
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-25 16:58:49 +0100

    Update web construction

--- src/bio2bel_interpro/web.py
+++ src/bio2bel_interpro/web.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+""" This module contains the flask application to visualize the db
+
+when pip installing
+
+.. source-code:: sh
+
+    pip install bio2bel_interpro[web]
+
+"""
 
 import flask_admin
 from flask import Flask
@@ -8,13 +17,20 @@ from flask_admin.contrib.sqla import ModelView
 from bio2bel_interpro.manager import Manager
 from bio2bel_interpro.models import *
 
-app = Flask(__name__)
-admin = flask_admin.Admin(app, url='/')
 
-manager = Manager()
+def create_application(connection=None, url=None):
+    app = Flask(__name__)
+    manager = Manager(connection=connection)
+    add_admin(app, manager.session, url=url)
 
-admin.add_view(ModelView(Family, manager.session))
-admin.add_view(ModelView(Protein, manager.session))
+
+def add_admin(app, session, url=None):
+    admin = flask_admin.Admin(app, url=(url or '/'))
+    admin.add_view(ModelView(Family, session))
+    admin.add_view(ModelView(Protein, session))
+    return admin
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    interpro_app = create_application()
+    interpro_app.run(debug=True, host='0.0.0.0', port=5000)
