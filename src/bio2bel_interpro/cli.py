f367e4529428e0deebd7e5a32db1689f63c68479
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-19 08:08:19 +0100

    Update CLI and web module

--- src/bio2bel_interpro/cli.py
+++ src/bio2bel_interpro/cli.py
@@ -20,10 +20,13 @@ log = logging.getLogger(__name__)
 
 
 @click.group()
-def main():
+@click.option('-c', '--connection', help='Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
+@click.pass_context
+def main(ctx, connection):
     """InterPro to BEL"""
     log.setLevel(logging.INFO)
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    ctx.obj = Manager(connection=connection)
 
 
 @main.command()
@@ -55,29 +58,27 @@ def write_go_mapping(file):
 
 
 @main.command()
-@click.option('-c', '--connection', help='Connection to cache. Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
-def populate(connection):
+@click.pass_obj
+def populate(manager):
     """Populates the database"""
-    manager = Manager(connection=connection)
     manager.populate()
 
 
 @main.command()
-@click.option('-c', '--connection', help='Connection to cache. Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
 @click.option('-y', '--yes', is_flag=True)
-def drop(connection, yes):
+@click.pass_obj
+def drop(manager, yes):
     """Drops the database"""
-    manager = Manager(connection=connection)
     if yes or click.confirm('Drop database?'):
         manager.drop_all()
 
 
 @main.command()
-@click.option('-c', '--connection', help='Connection to cache. Defaults to {}'.format(DEFAULT_CACHE_CONNECTION))
-def web(connection):
+@click.pass_obj
+def web(manager):
     """Run the web app"""
-    from .web import create_application
-    app = create_application(connection=connection, url='/')
+    from .web import get_app
+    app = get_app(connection=manager, url='/')
     app.run(host='0.0.0.0', port=5000)
 
 
